500a393b5708f87c431d364bd345f78f32e6dd48
ive-m <ivesita.maria@gmail.com>
2022-09-24 05:35:44 -0400

    Comment fixed

--- lotide/eqArrays.js
+++ lotide/eqArrays.js
@@ -19,7 +19,7 @@ const eqArrays = function(first, second) {
 
 console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
 console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
-console.log(eqArrays(["3", "2", "3", "4"], ["1", "2", "3", "4"])); // => true
+console.log(eqArrays(["3", "2", "3", "4"], ["1", "2", "3", "4"])); // => false
 console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]));// => false
 
 assertEqual(eqArrays([0, 2, 6, 8], [0, 2, 3]), false);
