495b14379d711219d99842097ee0c7d6d1b3500b
ive-m <ivesita.maria@gmail.com>
2022-10-14 20:46:43 -0400

    Refactoring

--- lotide/assertArraysEqual.js
+++ lotide/assertArraysEqual.js
@@ -20,8 +20,6 @@ const assertArraysEqual = function(array1, array2) {
   }
 };
 
-assertArraysEqual([], ["1 ", "2 ", "ho"]);// failed
-assertArraysEqual([1, 2, 3], ["1 ", "2 ", "3"]);// failed
-assertArraysEqual(["ho", "ho", "ho"], [1, "2 ", "ho"]);// failed
-assertArraysEqual(["ho", "ho", "ho"], ["ho", "ho", "ho"]);// passed
-assertArraysEqual(["ho", "ho", "ho", "ho"], ["ho", "ho", "ho"]);// passed
+
+
+module.exports = assertArraysEqual;
--- lotide/tail.js
+++ lotide/tail.js
@@ -1,10 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} ===  ${expected}`);
-  } else console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-};
 
 const tail = function(array) {
   let newArray = array.slice(1);
   return newArray;
 };
+
+module.exports= tail;
